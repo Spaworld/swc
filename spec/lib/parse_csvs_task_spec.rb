0bd2b17342871e8ee059f56a5b26111578fd3351
Dimitry Nazarov <dimitrynazarov@gmail.com>
2016-03-24 01:12:37 -0400

    inits the parse_csvs rake task spec

---
+++ spec/lib/parse_csvs_task_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+describe 'parse_csvs task' do
+
+  it 'finds csvs' do
+    pending 'init'
+  end
+
+  it 'successfully parses and updates the Channels table' do
+    pending 'init'
+  end
+
+  it 'successfully parses and updates the Orders table' do
+    pending 'init'
+  end
+
+  it 'successfully parses and updates the OrderDetails table' do
+    pending 'init'
+  end
+
+  it 'successfully parses and updates the Products table' do
+    pending 'init'
+  end
+
+  it 'skips invalid records gracefully' do
+    pending 'init'
+  end
+
+end
